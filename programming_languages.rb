0eb04f8ae7c29c60aa762cebe715619eded5a7d1
Emily R <emilyroulund@gmail.com>
2019-05-06 19:17:12 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,12 @@
 def reformat_languages(languages)
   new_hash = {} 
   languages.each do |style, data| 
-    data.each do |lang_name, describe| 
+          #(style is either oo or functional)
+    data.each do |lang_name, types| 
+          #lang_name = ruby, javascript, python, java; OR clojure, erlang, scala, javascript 
+          # type = interpreted or compiled, 
        if new_hash[lang_name] == nil
-          new_hash[lang_name] = describe
+          new_hash[lang_name] = types
           new_hash[lang_name][:style] = []
         end
         new_hash[lang_name][:style] << style
